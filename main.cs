045e95aa1dee9e19042fbcc6b7b99a64761cd63e
tarikjecar123 <bbasilisk16@gmail.com>
2020-11-08 23:11:30 +0000

    sifrovanje, reflektor, rotacija rotora

--- main.cs
+++ main.cs
@@ -1,13 +1,11 @@
 using System;
 class MainClass 
 {
-  // odabir pocetka rotora
-  // odabir rotora
+  //nizovi--------------------------------------------------------------------------
   static string[] Abeceda ={ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
   static string[] RotorBETA = {"L","E","Y","J","V","C","N","I","X","W","P","B","Q","M","D","R","T","A","K","Z","G","F","U","H","O","S"};
   static string[] RotorGAMA = {"F","S","O","K","A","N","U","E","R","H","M","B","T","I","Y","C","W","L","Q","P","Z","X","V","G","J","D"};
-  //static string[] Reflektor1 = {"E","N","K","Q","A","U","Y","W","J","I","C","O","P","B","L","M","D","X","Z","V","F","T","H","R","G","S"};
-  //static string[] Reflektor2 = {"R","D","O","B","J","N","T","K","V","E","H","M","L","F","C","W","Z","A","X","G","Y","I","P","S","U","Q"};
+  //unos----------------------------------------------------------------------------
   public static string[] Unos(string [] Reflektori)
   {
     string[] a= new string[26];
@@ -20,27 +18,18 @@ class MainClass
     }
     return a;
   }
+  //pomeranje rotora-----------------------------------------------------------------
   static string[] Pomeranje_niza(string [] niz)
   {
-    niz = new string [26];
-    string j = " ";
-    string k = niz[0];
-    for(int i = 1; i<niz.Length;i++)
+    string [] RotorAlfa = new string[26];
+    RotorAlfa[0]=niz[25];
+    for (int i = 25;i>0;i--)
     {
-      if(i!=niz.Length-1)
-      {
-        j = niz[i];
-        niz[i]=k;
-        k = j;
-      }
-      else
-      {
-        niz[0]=niz[25];
-        niz[25]=j;
-      }
-    }
-    return niz; 
+      RotorAlfa[i]=niz[i-1];
+    } 
+    return RotorAlfa;
   }
+  //pronalazenje elemenata ka reflektoru----------------------------------------------
   static int Isti_element(string slovo)
   {
     int pozicija = 0;
@@ -50,6 +39,17 @@ class MainClass
     }
     return pozicija;
   }
+  //pronalazenje elemenata od reflektora ka outputu-------------------------------------
+  static int Isti_element_povratak(string slovo, string [] Rotor)
+  {
+    int pozicija = 0;
+    for (int i=0; i<Rotor.Length; i++)
+    {
+      if (Rotor[i]==slovo) pozicija=i;
+    }
+    return pozicija;
+  }
+  //-------------------------------------------------------------------------------------
   public static void Main (string[] args)
   {       
     l1:
@@ -63,7 +63,7 @@ class MainClass
     l3:
     Console.WriteLine("Pogresan unos probajte ponovo!");
     l4:
-    //Unos reci
+    //Unos reci---------------------------------------------------------------------------
     Console.Write("Unesite rec: ");
     string x = Console.ReadLine();
     x = x.ToUpper();
@@ -85,8 +85,8 @@ class MainClass
     goto l2;
     reset:
     Console.WriteLine("Pogresili ste tokom unosa. Molim vas krenite od pocetka! ");
+    Console.WriteLine("");
     l2:
-    
     Console.WriteLine("Da li zelite da podesite reflektor?  (DA/NE) ");
     string str = Console.ReadLine();
     str = str.ToUpper();
@@ -97,7 +97,8 @@ class MainClass
       reflektor_osnova[i] = "";
       reflektor_preslikano[i] = "";
     }
-     Console.WriteLine("Unesite elemente preslikavanja u obliku (A B), a se prelikava u b. ");
+    //elementi koji se preslikavaju (reflektor)--------------------------------------------------------
+     Console.WriteLine("Unesite elemente preslikavanja u obliku (A B) ");
      for (int z = 0; z<13;z++)
      {
        UnoseElemenataReflektora = Console.ReadLine();
@@ -105,7 +106,7 @@ class MainClass
        reci = UnoseElemenataReflektora.Split(" ");
        for (int t = 0; t<2;t++)
        {
-         if ((reci[t] != "A")&&(reci[t] != "B")&&(reci[t] != "C")&&(reci[t] != "D")&&(reci[t] != "E")&&(reci[t] != "F")&&(reci[t] != "G")&&(reci[t] != "H")&&(reci[t] != "I")&&(reci[t] != "J")&&(reci[t] != "K")&&(reci[t] != "L")&&(reci[t] != "M")&&(reci[t] != "N")&&(reci[t] != "O")&&(reci[t] != "P")&&(reci[t] != "Q")&&(reci[t] != "R")&&(reci[t] != "S")&&(reci[t] != "T")&&(reci[t] != "U")&&(reci[t] != "V")&&(reci[t] != "W")&&(reci[t] != "X")&&(reci[t] != "Y")&&(reci[t] != "Z"))
+        if ((reci[t] != "A")&&(reci[t] != "B")&&(reci[t] != "C")&&(reci[t] != "D")&&(reci[t] != "E")&&(reci[t] != "F")&&(reci[t] != "G")&&(reci[t] != "H")&&(reci[t] != "I")&&(reci[t] != "J")&&(reci[t] != "K")&&(reci[t] != "L")&&(reci[t] != "M")&&(reci[t] != "N")&&(reci[t] != "O")&&(reci[t] != "P")&&(reci[t] != "Q")&&(reci[t] != "R")&&(reci[t] != "S")&&(reci[t] != "T")&&(reci[t] != "U")&&(reci[t] != "V")&&(reci[t] != "W")&&(reci[t] != "X")&&(reci[t] != "Y")&&(reci[t] != "Z"))
          {
            goto reset;
          }
@@ -123,8 +124,9 @@ class MainClass
        reflektor_preslikano[z] = reci[1];
      }
     }
-    else if (str == "NE"){}
+    else if (str == "NE"){goto skipper;}
     else {Console.WriteLine("Pogresan unos! Probaj Opet!"); goto l2;}
+    skipper:
     Console.WriteLine("Trenutna podesavnja reflektora: ");
       for (int i = 0; i<15;i++)
       {Console.Write("*");}
@@ -143,6 +145,72 @@ class MainClass
         Console.Write("*");
       }
       Console.WriteLine("");
+      //zamena rotora------------------------------------------------------------------------------
+      Console.WriteLine("Da li zelite da promenite rotore? (upisite DA ako zelite)");
+      string promenarotora = Console.ReadLine();
+      promenarotora = promenarotora.ToUpper();
+      if (promenarotora == "DA")
+      {
+        nastavi:
+        Console.WriteLine("Koji rotor zelite da menjate? (PRVI/DRUGI)");
+        string promenarotora2 = Console.ReadLine();
+        
+        promenarotora2 = promenarotora2.ToUpper();
+        if (promenarotora2 == "PRVI")
+        {
+          Console.WriteLine("Sa kojim rotorom zelite da ga zamenite? (BETA/GAMA) ");
+          string promenarotora3 = Console.ReadLine();
+          if (promenarotora3 == "BETA")
+          {
+            for(int i = 0; i<26;i++)
+            {
+            PrviRotor[i]=RotorBETA[i];
+            }
+          }
+          if (promenarotora3 == "DRUGI")
+          {
+            for(int i = 0; i<26;i++)
+            {
+            PrviRotor[i]=RotorGAMA[i];
+            }
+          }
+        }
+        else if (promenarotora2 == "DRUGI")
+        {
+          Console.WriteLine("Sa kojim rotorom zelite da ga zamenite? (BETA/GAMA) ");
+          string promenarotora3 = Console.ReadLine();
+          if (promenarotora3 == "BETA")
+          {
+            for(int i = 0; i<26;i++)
+            {
+            DrugiRotor[i]=RotorBETA[i];
+            }
+          }
+          if (promenarotora3 == "DRUGI")
+          {
+            for(int i = 0; i<26;i++)
+            {
+            DrugiRotor[i]=RotorGAMA[i];
+            }
+          }
+        }
+        Console.WriteLine("Da li zelite da ponovo promenite neki rotor?");
+        promenarotora = Console.ReadLine();
+        promenarotora = promenarotora.ToUpper();
+        if(promenarotora=="DA")
+          goto nastavi;
+      }
+      Console.WriteLine("Na raspolaganju su rotori GAMA i BETA");
+      Console.WriteLine("rotor GAMA je: ");
+      for(int i = 0; i<26;i++)
+      {
+        Console.Write(RotorGAMA[i]+" ");
+      }
+      Console.WriteLine("rotor BETA je: ");
+      for(int i = 0; i<26;i++)
+      {
+        Console.Write(RotorBETA[i] + " ");
+      }
       Console.WriteLine("Osnovne pozicije rotora su: ");
       Console.Write("Rotor broj jedan: ");
       for (int i = 0; i<26; i++)
@@ -156,33 +224,7 @@ class MainClass
         Console.Write(DrugiRotor[i]+" ");
       }
       Console.WriteLine("");
-    
-//    string[] Reflektori = Unos(Reflektori);
-    /*
-    Console.WriteLine("Da li zelite da sifrujete. (DA/NE)");
-    string odabir = Console.ReadLine();
-    odabir = odabir.ToUpper();
-
-    if(odabir=="DA")
-    {
-      Console.WriteLine("da li zelite da promenite parametre? (DA/NE)");
-      string promenaParametara = Console.ReadLine();
-        if (promenaParametara == "DA")
-        {
-          
-        }
-    }
-    else if (odabir=="NE")
-    {
-       Console.WriteLine("tokom desifrovanja neophodno je zameniti parametre: ");
-        Console.WriteLine("Unesi elemente prvog rotora: ");
-      PrviRotor = Unos(PrviRotor);
-        Console.WriteLine("Unesi elemente drugog rotora: ");
-      DrugiRotor = Unos(DrugiRotor);
-        Console.WriteLine("Unesi raspored reflektora: ");
-     // Reflektor = Unos(Reflektor);
-    }
-    */
+    //promena pocetka rotora-------------------------------------------------------------------
     Console.WriteLine("Da li zelite da promenite pocetak rotora? (DA/NE)");
     string pocetak = Console.ReadLine();
     pocetak = pocetak.ToUpper();
@@ -205,42 +247,57 @@ class MainClass
       for(int i = 0; i<h;i++)
         Pomeranje_niza(DrugiRotor);
     }
+    //konverzija, sifrovanje/desifrovanje----------------------------------------------
     string slovo;
     int pozicija, brojac=1;
+    int element_iz_reflektora;
+    bool reflektor=true;
     for(int i = 0; i<rec.Length; i++)
     {
-      Pomeranje_niza(PrviRotor);
+      PrviRotor=Pomeranje_niza(PrviRotor);
+      brojac++;
       if(brojac==26)
       {
-        Pomeranje_niza(DrugiRotor);
+        DrugiRotor=Pomeranje_niza(DrugiRotor);
         brojac=0;
       }
-      brojac++;
       slovo = rec[i];
       pozicija = Isti_element(slovo);
       slovo = PrviRotor[pozicija];
       pozicija = Isti_element(slovo);
       slovo = DrugiRotor[pozicija];
-      
-      Console.WriteLine(slovo);
+      for (int j=0; j<reflektor_osnova.Length; j++)
+      {
+        if (slovo==reflektor_osnova[j]) 
+        {
+          pozicija = j;
+          slovo = reflektor_preslikano[j];
+          reflektor=false;
+        }
+      }
+      if (reflektor)
+      {
+        for (int znj=0; znj<reflektor_preslikano.Length; znj++)
+        {
+          if (slovo==reflektor_preslikano[znj]) 
+          {
+            pozicija = znj;
+            slovo = reflektor_osnova[znj];
+          }
+        }
+      }
+      reflektor=true;
+      pozicija = Isti_element_povratak(slovo, DrugiRotor);
+      slovo = Abeceda[pozicija];
+      pozicija = Isti_element_povratak(slovo, PrviRotor);
+      slovo = Abeceda[pozicija];
+      Console.Write(slovo);
     }
     Console.WriteLine("Da li želite da završite sa programom? (DA/NE)");
     string exit = Console.ReadLine();
     exit = exit.ToUpper();   
     if(exit == "NE")
     goto l1; 
-    Console.WriteLine("~~~~~ Hvala na koriscenju DrimTimEnigma emulatora! ~~~~~~~~~~~");
+    Console.WriteLine("~~~~~~~~ Hvala na koriscenju DrimTimEnigma emulatora! ~~~~~~~~");
   }
 }
-/*
-{
-            string[] Rotor1 = {L,E,Y,J,V,C,N,I,X,W,P,B,Q,M,D,R,T,A,K,Z,G,F,U,H,O,S,},
-
-            string[] Rotor2 ={F,S,O,K,A,N,U,E,R,H,M,B,T,I,Y,C,W,L,Q,P,Z,X,V,G,J,D,},
-
-
-            string[] Rotor2 ={E,N,K,Q,A,U,Y,W,J,I,C,O,P,B,L,M,D,X,Z,V,F,T,H,R,G,S,},
-            
-            string[] Rotor3 ={R,D,O,B,J,N,T,K,V,E,H,M,L,F,C,W,Z,A,X,G,Y,I,P,S,U,Q,},
-};
-*/
